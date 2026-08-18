83c8a1dd302fc00369a7af4afbec7f136bd3aac8
Abusayed Shuvo <abusayedshuvo24@gmail.com>
2026-08-18 22:37:50 +0600

    function part done

--- js/script.js
+++ js/script.js
@@ -7,3 +7,74 @@ for (let item of heartBtn) {
     heartCountElement.innerHTML = heartCount;
   });
 }
+
+// Time function part
+
+const now = new Date();
+
+const hours = now.getHours();
+const minutes = now.getMinutes();
+const seconds = now.getSeconds();
+const ampm = hours >= 12 ? "PM" : "AM";
+const formattedHours = hours % 12 || 12;
+const formattedTime = `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}  ${ampm}`;
+
+// call btn function part
+const coin = document.getElementById("coin");
+const callBtn = document.querySelectorAll(".call-btn");
+const callHistory = document.getElementById("call-history");
+
+for (let item of callBtn) {
+  item.addEventListener("click", function (event) {
+    const title = event.target.parentNode.parentNode.querySelector(
+      ".text-2xl.font-semibold",
+    ).innerText;
+    const number = event.target.parentNode.parentNode.querySelector(
+      ".font-bold.text-3xl",
+    ).innerText;
+
+    if (coin.innerText < 20) {
+      alert("Your Coin is finished");
+      return;
+    }
+    coin.innerText = parseInt(coin.innerText) - 20;
+    alert(`${title} : ${number} `);
+
+    const itemDiv = document.createElement("div");
+    itemDiv.innerHTML = `
+    <div class="bg-[#FAFAFA] p-4 rounded-lg flex justify-between mb-3">
+              <div>
+                <p class="font-semibold">${title}</p>
+                <p class="text-gray-600"> ${number} </p>
+              </div>
+              <p class=""> ${formattedTime}</p>
+    </div>
+    `;
+    callHistory.appendChild(itemDiv);
+  });
+}
+
+// clear btn function part
+
+document.getElementById("btn-clear").addEventListener("click", function () {
+  callHistory.innerHTML = "";
+});
+
+// copy btn function part
+const copyCountEl = document.getElementById("copy-count");
+let copyCount = parseInt(copyCountEl.innerText);
+
+const copyBtns = document.querySelectorAll(".copy-btn");
+for (let item of copyBtns) {
+  item.addEventListener("click", function (event) {
+    copyCount++;
+    copyCountEl.innerText = copyCount;
+
+    const number = event.target.parentNode.parentNode.querySelector(
+      ".font-bold.text-3xl",
+    ).innerText;
+    navigator.clipboard.writeText(number).then(() => {
+      alert("Hotline number is copied");
+    });
+  });
+}
--- readme.md
+++ readme.md
@@ -1,19 +1,45 @@
 ## Emergency Hotline
 
-### 1. Navbar
+### Navbar
 
-### 2. Hero Section
+### Hero Section
 
-### 2. Main Section
+### Main Section
 
 ### Emergency Hotline Section
 
 ### History Section
 
-### 3. Responsiveness
+### Responsiveness
 
 ---
 
 ## Functionalities
 
 - Clicking on the 💗 **heart icon** of any card will increase the count in the Navbar
+
+### Call Buttons
+
+- On clicking a card's **Call Button**, following actions will happen:
+  - Show an **alert** with a message including the service name and number
+  - Each call will **cut 20 coins**. Reduce Coin after each click.
+  - If coins are less than 20, show a relevant alert and terminate the process.
+  - Add this service into the **Call History section** with:
+    - Service name
+    - Service number
+
+    ***
+
+### Call History Section
+
+- Show all called services with name & number. This will empty initially. when call button clicked it will filled dynamically.
+- A **Clear History button** on the right
+- Clicking this button will remove all data from call history
+
+---
+
+### 🧪 Challenges Part
+
+- On clicking the **Copy button**, show an alert and **increase the copy count**
+
+- Hotline number will be **copied on click** so it can be pasted anywhere
